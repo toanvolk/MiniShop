2387af29b435dc150825730b1fe351ceb068ff27
34018 <toanvolk@gmail.com>
2020-11-09 17:48:31 +0700

    step 2 pagination

--- MiniShop.Web/Controllers/HomeController.cs
+++ MiniShop.Web/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MiniShop.App;
 using MiniShop.Web.Models;
 
 namespace MiniShop.Web.Controllers
@@ -12,17 +14,33 @@ namespace MiniShop.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService  productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
         public IActionResult Index()
         {
+            //var productDtos = _productService.LoadDataPage(page, pageSize);
+
+            //----------------
+            //var model = new Tuple<Tuple<ICollection<ProductDto>, int>>(productDtos);
             return View();
         }
 
+        public IActionResult ProductPage(int pageNumber = 1, int pageSize = 9)
+        {
+            var productDtos = _productService.LoadDataPage(pageNumber, pageSize);
+            var model = new
+            {
+                source = productDtos.Item1,
+                total = productDtos.Item2
+            };
+            return Json(model);
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ MiniShop.Web/wwwroot/js/client/page-home.js
@@ -0,0 +1,61 @@
+﻿(function ($, document){
+    let _paginationClass = ".mnshop-pagination";
+    let _formatTemplate = function (data) {
+        let _template = `
+            <div class="col-md-4 text-center animate-box">
+                <div class="product">
+                    <div class="product-grid" style="background-image:url({#:picture});">
+                        <div class="inner">
+                            <p>
+                                <a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a>
+                                <a href="single.html" class="icon"><i class="icon-eye"></i></a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <h3><a href="{#:trackingLink}">{#:name}</a></h3>
+                        <span class="price">{#:price}</span>
+                    </div>
+                </div>
+            </div>
+                        `;
+        _template = _template
+            .replaceAll(new RegExp("{#:picture}", "gi"), data.picture)
+            .replaceAll(new RegExp("{#:name}", "gi"), data.name)
+            .replaceAll(new RegExp("{#:trackingLink}", "gi"), data.trackingLink)
+            .replaceAll(new RegExp("{#:price}", "gi"), data.price)
+        return _template;
+    }
+    //$(_productPageId).load("product");
+    $(_paginationClass).pagination({
+        dataSource: 'home/productpage',
+        locator: 'source',
+        pageNumber: 1,
+        totalNumberLocator: function (response) {
+            // you can return totalNumber by analyzing response content
+            return response.total;
+        },
+        pageSize: 2,
+        ajax: {
+            beforeSend: function () {
+
+            }
+        },
+        callback: function (data, pagination) {
+            // template method of yourself
+            //var html = template(data);
+            //dataContainer.html(html);
+            console.log(data, pagination);
+            let _$containPagination = $(pagination.el).closest('.row');
+            let _$containRoot = $(pagination.el).closest('.mshop-product-client');
+            let _cardId = $(_$containPagination).data("cardId");
+
+            let _html = '';
+            $(data).each(function (index, value) {
+                _html += _formatTemplate(value);
+            });
+            $(_$containRoot).find('#' + _cardId).html(_html);
+            //contentWayPoint();
+        }
+    });
+}($, document));
