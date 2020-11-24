22ae848cd7cbaff486c6e75066cd88f711232e0e
Toàn Võ <toanvolk@gmail.com>
2020-11-24 21:15:59 +0700

    Chỉnh sửa share facebook

--- MiniShop.Web/Controllers/ProductController.cs
+++ MiniShop.Web/Controllers/ProductController.cs
@@ -21,10 +21,11 @@ namespace MiniShop.Web.Controllers
         {
             //filter code => productId
             var productId = _productService.GetProductId(code);
+            var productDto = _productService.GetData(productId);
             if (productId == Guid.Empty) return Redirect("/html/not-found.html");
             var useHostAddress = this.HttpContext.Connection.RemoteIpAddress.ToString();
             _productService.CountClick(productId, useHostAddress, string.Empty);
-            return View("Index", productId);
+            return View("Index", productDto);
         }
         [Route("data/{productId}")]
         public IActionResult ProductView(Guid productId)
