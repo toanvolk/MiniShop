9e48cdaddb0904d1ba0242c0c91e6f90bf88c4d0
Toàn Võ <toanvolk@gmail.com>
2020-11-28 00:44:33 +0700

    finish

--- MiniShop.Web/wwwroot/js/client/page-home.js
+++ MiniShop.Web/wwwroot/js/client/page-home.js
@@ -8,6 +8,7 @@
     let _btn_categorys = ".btn-categorys";
     var timeDelay;
     var numberDeplayLazy = 0;
+    var numberCountMax = 0;
 
     let _formatCardTemplate = function (data) {
         let _template = `
@@ -122,70 +123,11 @@
 
         $(_mshop_filter + ' .loader').show();
         timeDelay = setTimeout(function () {
-            _genericPagination();
+            //_genericPagination();
+            _loadLazy(true);
             $(_mshop_filter + ' .loader').hide();
         }, 2500);
     });
-    //
-    let _genericPagination = function () {
-        let _url = 'home/productpage{#:_paramStrs}';
-        let _paramStrs = "";
-        let _content = $(_paginationClass);
-        //get values
-        let _values = [];
-        $(_mshop_product_client + ' ' + _btn_categorys + ' a.active').each(function (index, item) {
-            let _id = $(item).data("id");
-            if (_id != "ALL")
-                _values.push($(item).data("id"));
-        });
-        let _textSearch = $(_mshop_filter).find(_btnProductSearch).first().val();
-        //parse filter
-        if (_textSearch || _values) {
-            if ($(_content).data('pagination')) _content.pagination('destroy');
-            let _paramObject = {
-                TextSearch: _textSearch,
-                CategoryIds: _values
-            }
-            _paramStrs = "?paramStrs=" + JSON.stringify(_paramObject);
-        };
-
-        _url = _url.replaceAll(new RegExp("{#:_paramStrs}", "gi"), _paramStrs || "");
-
-        _content.pagination({
-            dataSource: _url,
-            className: 'paginationjs-big',
-            locator: 'source',
-            pageNumber: 1,
-            totalNumberLocator: function (response) {
-                // you can return totalNumber by analyzing response content
-                return response.total;
-            },
-            pageSize: cdC.pageSize,
-            ajax: {
-                beforeSend: function () {
-
-                }
-            },
-            callback: function (data, pagination) {
-                let _$containPagination = $(pagination.el).closest('.row');
-                let _$containRoot = $(pagination.el).closest(_mshop_product_client);
-                let _cardId = $(_$containPagination).data("cardId");
-
-                let _html = '';
-                $(data).each(function (index, value) {
-                    _html += _formatCardTemplate(value);
-                });
-
-                if ($(_btnProductSearch).val()) {
-                    let _resultText = "Tìm thấy {#:totalItem} kết quả";
-                    _resultText = _resultText.replaceAll(new RegExp("{#:totalItem}", "gi"), pagination.totalNumber);
-                    _captionSearchResult(_resultText);
-                }
-
-                $(_$containRoot).find('#' + _cardId).html(_html);
-            }
-        });
-    }
     let _genericHero = async function () {
         let _url = 'home/producthero';
         let _html = '';
@@ -214,7 +156,13 @@
     let _captionSearchResult = function (text) {
         $(_mshop_filter + ' ' + _search_result).text(text);
     }
-    let _loadLazy = function () {
+    let _loadLazy = function (reload) {
+        let _cardId = $(".mshop-filter").data("cardId");
+        if (reload) {
+            numberDeplayLazy = 0;
+            numberCountMax = 0;
+            $(_mshop_product_client).find('#' + _cardId).html('');
+        }
         let _url = 'home/productpage{#:_paramStrs}';
         let _paramStrs = "";
         //get values
@@ -226,7 +174,7 @@
         });
         let _textSearch = $(_mshop_filter).find(_btnProductSearch).first().val();
         //parse filter
-        if (_textSearch || _values) {
+        if (_textSearch || _values != []) {
             let _paramObject = {
                 TextSearch: _textSearch,
                 CategoryIds: _values,
@@ -238,26 +186,28 @@
         _url = _url.replaceAll(new RegExp("{#:_paramStrs}", "gi"), _paramStrs || "");
 
         $.get(_url, {}, function (res) {
-            let _cardId = $(".mshop-filter").data("cardId");
-
             let _html = '';
             $(res.source).each(function (index, value) {
                 _html += _formatCardTemplate(value);
             });
-
+            //set total row into numberCountMax
+            numberCountMax = res.total;
             if ($(_btnProductSearch).val()) {
                 let _resultText = "Tìm thấy {#:totalItem} kết quả";
                 _resultText = _resultText.replaceAll(new RegExp("{#:totalItem}", "gi"), res.total);
                 _captionSearchResult(_resultText);
             }
             //cập nhật tham số load lazy
-            numberDeplayLazy += cdC.pageSize;
+            numberDeplayLazy += res.source.length;
             $(_mshop_product_client).find('#' + _cardId).append(_html);
+
+            console.log("numberDeplayLazy: " + numberDeplayLazy, "numberCountMax :" + numberCountMax)
         });
+
+
     }
     // init
     _genericHero();
-    //_genericPagination();
     _genericCategoryButton();
     $(_mshop_filter + ' .loader').hide();
     //event
@@ -285,17 +235,22 @@
             $(_mshop_product_client + ' ' + _btn_categorys + ' a[data-id=ALL]').removeClass("active");
         }
 
-        _genericPagination();
+        _loadLazy(true);
     })
     //scroll
     $(window).scroll(function () {
-        if (($(window).scrollTop() >= $(document).height() - $(window).height() - $("#fh5co-footer")[0].offsetHeight)
-            && ($(window).scrollTop() <= $(document).height() - $(window).height() - $("#fh5co-footer")[0].offsetHeight  + 20)
-        ) {
-            console.log('$(window).scrollTop() ' + $(window).scrollTop());
-            console.log('$(document).height() ' + $(document).height());
-            console.log('$(window).height() ' + $(window).height());
-            _loadLazy();
+        if (($(window).scrollTop() >= $(document).height() - $(window).height() - $("#fh5co-footer")[0].offsetHeight)) {   
+            clearTimeout(timeDelay);
+            if (numberDeplayLazy == numberCountMax && numberDeplayLazy != 0) {
+                $(_mshop_product_client + ' .loader-tomato').hide();
+                return;
+            }
+            $(_mshop_product_client + ' .loader-tomato').show();
+            timeDelay = setTimeout(function () {
+                _loadLazy();
+                $(_mshop_product_client + ' .loader-tomato').hide();
+            }, 2200);
+            
         }
     });
 }($, document));
