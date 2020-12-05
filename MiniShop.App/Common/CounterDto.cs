51ece81101da08bbfa35ec79aa2096f11b214ee3
Toàn Võ <toanvolk@gmail.com>
2020-12-05 15:32:35 +0700

    1. finish services

---
+++ MiniShop.App/Common/CounterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniShop.App.Common
+{
+    public class CounterDto
+    {
+        public int Viewer { get; set; }
+    }
+}
