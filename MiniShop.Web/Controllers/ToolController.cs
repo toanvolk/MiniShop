5679cdb97607c69974f6650e18faf0dce5aa9f81
Toàn Võ <toanvolk@gmail.com>
2020-12-24 22:39:06 +0700

    step 1

---
+++ MiniShop.Web/Controllers/ToolController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MiniShop.App;
+
+namespace MiniShop.Web.Controllers
+{
+    [Route("tool")]
+    public class ToolController : BaseController
+    {
+        private readonly InfoServerConfig _infoServerConfig;
+        private readonly IProductService _productService;
+
+        public ToolController(IBaseService baseService, IBlogService blogService
+            , IOptions<InfoServerConfig> optionAccessor
+            , IProductService productService) : base(baseService)
+        {
+            _infoServerConfig = optionAccessor.Value;
+            _productService = productService;
+        }
+        [Route("posts")]
+        public IActionResult Posts()
+        {
+            //var model = new Tuple<InfoServerConfig>(_infoServerConfig);
+            return View();
+        }
+
+    }
+}
